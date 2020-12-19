37039c0aefffea824c6863744d1e3577549130cd
KimVal <71520455+Valeria19k@users.noreply.github.com>
2020-12-19 14:31:22 +0300

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Test {
+    public static void main(String[] args){
+        Array s= new Array();
+        s.fill();
+        Linked l = new Linked();
+        l.fill();
+    }
+}
